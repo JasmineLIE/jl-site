5186c98e0bc9282ced73ae5dd04f095965f242ac
Lusa <jasminely428@gmail.com>
2023-12-24 14:59:56 -0500

    Initial commit

---
+++ src/lib/slices/HeroText/index.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { PrismicRichText } from "@prismicio/svelte";
+
+	/** @type {import("@prismicio/client").Content.HeroTextSlice} */
+	export let slice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<PrismicRichText field={slice.primary.title} /> 
+	<PrismicRichText field={slice.primary.subtitle} />
+	<!--to let the HUB editor changes be made onto the simulation in Slicemachine-->
+</section>
+
+<style>
+	section {
+	
+		background-size: cover;
+		text-transform: uppercase;
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		justify-content: center;
+		gap: var(--size-4);
+		padding: var(--size-14) 0 var(--size-13);
+		
+
+	}
+
+	section :global(h1) {
+color: var(--sand-1);
+text-align: right;
+font-size: var(--size-11);
+line-height: var(--size-11);
+width: min-content;
+	}
+
+	section :global(p) {
+		color: var(--sand-9);
+		text-align: left;
+		font-size: var(--size-7);
+		line-height: 120%;
+		width: min-content;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,66 @@
+<script>
+	import { PrismicPreview } from '@prismicio/svelte/kit';
+	import { page } from '$app/stores';
+	import { repositoryName } from '$lib/prismicio';
+</script>
+
+<svelte:head>
+	<title>{$page.data.title}</title>
+	{#if $page.data.meta_description}
+		<meta name="description" content={$page.data.meta_description} />
+	{/if}
+	{#if $page.data.meta_title}
+		<meta name="og:title" content={$page.data.meta_title} />
+	{/if}
+	{#if $page.data.meta_image}
+		<meta name="og:image" content={$page.data.meta_image.url} />
+		<meta name="twitter:card" content="summary_large_image" />
+	{/if}
+</svelte:head>
+<main>
+	<slot />
+</main>
+<PrismicPreview {repositoryName} />
+
+<style>
+	@import "open-props/style";
+	@import "open-props/normalize";
+	@import "open-props/gray-hsl";
+
+	@font-face {
+		font-family: 'Anton';
+		font-style: normal;
+		font-weight: 100 900;
+		font-display: swap;
+		src: local(''), url('/Anton.woff2') format('woff2');
+		unicode-range: U+20-7E, U+E4, U+E5, U+F6;
+	}
+
+	/* Define colors scheme for light and dark mode */
+	:root {
+		--brand: var(--lime-10);
+		--text-1: var(--sand-9);
+		--text-2: var(--lime-11);
+		--surface-1: var(--sand-1);
+		--surface-2: var(--lime-1);
+		--surface-3: var(--lime-4);
+		--surface-4: var(--lime-4);
+		--surface-shadow: var(--gray-8-hsl);
+		--shadow-strength: 2%;
+		
+	}
+
+
+	:global(section) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--size-10);
+		padding: var(--size-11);
+	}
+
+	:global(section:nth-child(odd)) {
+		background: var(--sand-2);
+	}
+	
+</style>
